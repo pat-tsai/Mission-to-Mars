ae2668e3cba6d363a6f9d6bcd4e762f1a9d8b9d5
pat-tsai <67960210+pat-tsai@users.noreply.github.com>
2020-09-18 16:19:43 -0700

    deliverable 2 update
    updated web scraper app with full res photos

--- scraping.py
+++ scraping.py
@@ -17,7 +17,8 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
-        "last_modified": dt.datetime.now()
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_info()
     }
 
     # Stop webdriver and return data
@@ -81,7 +82,6 @@ def featured_image(browser):
 
     # Use the base url to create an absolute url
     img_url = f'https://www.jpl.nasa.gov{img_url_rel}'
-    print(img_url)
 
     return img_url
 
@@ -101,6 +101,56 @@ def mars_facts():
 
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
+
+
+def hemisphere_info():
+
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    browser = Browser('chrome', **executable_path)
+
+    # 1. Use browser to visit the URL
+    url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    browser.visit(url)
+
+    # 2. Create lists to hold the images and titles
+    hemisphere_info = []
+    picture_links = []
+    jpg_links = []
+
+    # base url used for navigation
+    base_url = 'https://astrogeology.usgs.gov'
+
+    # create soup object
+    soup_obj = soup(browser.html, "html.parser")
+
+    # create picture container that contains href links
+    picture_container = soup_obj.section.find_all('a', class_='itemLink product-item')
+    for picture in picture_container:
+        link = picture.get('href')
+
+        if link not in picture_links:
+            picture_links.append(link)
+
+    # navigate to each hemisphere page and grab full-size image link
+    for lin in picture_links:
+        href = base_url + f'{lin}'
+        browser.visit(href)
+
+        elems = browser.links.find_by_text('Sample')
+        for e in elems:
+            jpg_links.append(e['href'])
+
+    # creating container for hemisphere titles
+    title_container = soup_obj.section.find_all('h3')
+
+    # iterating through title container and img url list to create output dict
+    for i in range(len(title_container)):
+        title = (title_container[i].text)
+        img_url = jpg_links[i]
+        hemisphere_info.append({'img_url': img_url, 'title': title})
+
+    return hemisphere_info
+
 
 if __name__ == "__main__":
 
